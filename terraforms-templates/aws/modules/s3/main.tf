861b371a5a8634b59e4714373a57c542004f7342
leonardovd01 <67021947+leonardovd01@users.noreply.github.com>
2022-07-14 20:37:33 -0500

    Update main.tf

--- terraforms-templates/aws/modules/s3/main.tf
+++ terraforms-templates/aws/modules/s3/main.tf
@@ -6,25 +6,26 @@ resource "aws_s3_bucket" "bucket" {
   }
 
   tags = {
-    Name = "s3-data-bootcamp-jmru111292"
+    Name = "s3-data-bootcamp-leovd912823"
   }
 }
 
 resource "aws_s3_bucket_object" "object_movie" {
+  for_each = fileset("./data/", "**")
   bucket   = aws_s3_bucket.bucket.id
-  key      = "movie_reviews.csv"
-  source   = "data/movie_review.csv"
-  etag     = filemd5("data/movie_review.csv")
+  key      = each.value
+  source   = "./data/${each.value}"
+  etag     = filemd5("./data/${each.value}")
 }
-resource "aws_s3_bucket_object" "object_log" {
-  bucket   = aws_s3_bucket.bucket.id
-  key      = "log_reviews.csv"
-  source   = "data/log_reviews.csv"
-  etag     = filemd5("data/log_reviews.csv")
-}
-resource "aws_s3_bucket_object" "object_purchase" {
-  bucket   = aws_s3_bucket.bucket.id
-  key      = "user_purchase.csv"
-  source   = "data/user_purchase.csv"
-  etag     = filemd5("data/user_purchase.csv")
-}
+#resource "aws_s3_bucket_object" "object_log" {
+#  bucket   = aws_s3_bucket.bucket.id
+#  key      = "log_reviews.csv"
+#  source   = "data/log_reviews.csv"
+#  etag     = filemd5("data/log_reviews.csv")
+#}
+#resource "aws_s3_bucket_object" "object_purchase" {
+#  bucket   = aws_s3_bucket.bucket.id
+#  key      = "user_purchase.csv"
+#  source   = "data/user_purchase.csv"
+#  etag     = filemd5("data/user_purchase.csv")
+#}
